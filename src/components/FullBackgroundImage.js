94b99a441abce99f807a154d6bcea0c4f69894f6
Uli Schmid <uli@maruli.org>
2021-03-21 14:21:30 +0100

    add background image

---
+++ src/components/FullBackgroundImage.js
@@ -0,0 +1,38 @@
+import React from "react";
+import { graphql, useStaticQuery } from "gatsby";
+import BackgroundImage from "gatsby-background-image";
+
+const FullBackgroundImage = ({ children }) => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        desktop: file(relativePath: { eq: "horses-bg.jpeg" }) {
+          childImageSharp {
+            fluid(quality: 90, maxWidth: 1920) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+    `
+  );
+  const imageData = data.desktop.childImageSharp.fluid;
+  console.log(imageData);
+  return (
+    <BackgroundImage
+      Tag="section"
+      className=""
+      fluid={imageData}
+      backgroundColor={`#040e18`}
+      title="Fullscreen Background"
+      id="fullscreenbg"
+      role="img"
+      aria-label="Fullscreen Background"
+      preserveStackingContext={true}
+    >
+      {children}
+    </BackgroundImage>
+  );
+};
+
+export default FullBackgroundImage;
