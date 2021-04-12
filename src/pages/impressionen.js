91ec994cf0cfc40e4d61f20ce569fb1005257803
Uli Schmid <uli@maruli.org>
2021-04-12 20:51:56 +0200

    add impressions

--- src/pages/impressionen.js
+++ src/pages/impressionen.js
@@ -1,8 +1,20 @@
 import React from "react"
 import Layout from "../components/Layout"
+import SEO from "../components/SEO"
+import Hero from "../components/Hero"
 
 const ImpressionenPage = () => {
-  return <Layout>IMPRESSIONEN</Layout>
+  const data = {}
+  data.heading = "Impressionen"
+  data.text = ``
+  return (
+    <Layout>
+      <SEO></SEO>
+      <Hero data={data}></Hero>
+      <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+      <div className="elfsight-app-e2aba452-dcef-4b09-9056-2b03cc0af0d1"></div>
+    </Layout>
+  )
 }
 
 export default ImpressionenPage
