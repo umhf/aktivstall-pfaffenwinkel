c5ca5619c1338b5a770ec31450150746524bb218
Uli Schmid <uli@maruli.org>
2021-04-12 21:12:58 +0200

    try facebook plugin

--- src/pages/impressionen.js
+++ src/pages/impressionen.js
@@ -9,10 +9,38 @@ const ImpressionenPage = () => {
   data.text = ``
   return (
     <Layout>
+      <div id="fb-root"></div>
+      <script
+        async
+        defer
+        crossOrigin="anonymous"
+        src="https://connect.facebook.net/de_DE/sdk.js#xfbml=1&version=v10.0"
+        nonce="0HMK1WMI"
+      ></script>
       <SEO></SEO>
       <Hero data={data}></Hero>
-      <script src="https://apps.elfsight.com/p/platform.js" defer></script>
-      <div className="elfsight-app-e2aba452-dcef-4b09-9056-2b03cc0af0d1"></div>
+      <div
+        className="fb-page"
+        data-href="https://www.facebook.com/aktivstall.pfaffenwinkel/"
+        data-tabs="timeline"
+        data-width="500"
+        data-height=""
+        data-small-header="true"
+        data-adapt-container-width="false"
+        data-hide-cover="false"
+        data-show-facepile="true"
+      >
+        <blockquote
+          cite="https://www.facebook.com/aktivstall.pfaffenwinkel/"
+          className="fb-xfbml-parse-ignore"
+        >
+          <a href="https://www.facebook.com/aktivstall.pfaffenwinkel/">
+            Aktivstall Pfaffenwinkel
+          </a>
+        </blockquote>
+      </div>
+      {/* <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+      <div className="elfsight-app-e2aba452-dcef-4b09-9056-2b03cc0af0d1"></div> */}
     </Layout>
   )
 }
