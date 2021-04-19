b06de9f76d3eddd67b32310075d4a262758f22b5
Uli Schmid <uli@maruli.org>
2021-04-19 20:14:48 +0200

    upgraded lightwidget

--- src/pages/impressionen.js
+++ src/pages/impressionen.js
@@ -20,8 +20,8 @@ const ImpressionenPage = () => {
       <Hero data={data}></Hero>
       <div className="mb-12 mx-auto bg-black-trans p-4 blur">
         <iframe
-          src="//lightwidget.com/widgets/b625622304055c12957b9d6359039701.html"
-          scrolling="yes"
+          src="https://cdn.lightwidget.com/widgets/b625622304055c12957b9d6359039701.html"
+          scrolling="no"
           allowtransparency="true"
           className="lightwidget-widget"
           style={{ width: "100%", border: 0, overflow: "hidden" }}
