d10eee444b7a5c4b2bc0d84de4220e3e031044a2
Uli Schmid <uli@maruli.org>
2021-04-24 19:05:50 +0200

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,53 +1,22 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
+import Hero from "../components/Hero"
 
-// styles
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-
-// markup
 const NotFoundPage = () => {
+  const data = {}
+  data.heading = "Herrschaftsseiten"
+  data.text = ``
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
-          😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+    <Layout>
+      <SEO title="Impressum"></SEO>
+      <Hero data={data}></Hero>
+      <div className="mb-12 mx-auto bg-black-trans p-4 blur">
+        <p>die Seite gibt's leider net</p>
+        <Link to="/">Hier geht's zruck zur Startseitn.</Link>
+      </div>
+    </Layout>
   )
 }
 
